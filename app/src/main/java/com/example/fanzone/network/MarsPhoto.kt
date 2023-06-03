4411983e3a18e32cdc67678d37b4c4c1d85cfaa5
Yoel Nozar <ynozar@calpoly.edu>
2023-06-03 15:31:15 -0700

    add API dependencies

---
+++ app/src/main/java/com/example/fanzone/network/MarsPhoto.kt
@@ -0,0 +1,10 @@
+package com.example.fanzone.network
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class MarsPhoto(
+
+    val id: String,  @SerialName(value = "img_src") val imgSrc: String
+)
