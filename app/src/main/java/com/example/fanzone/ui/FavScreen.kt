cbead91446e4713fe9fd7c048c4ba4db29184248
arnav9181 <arnavbanthia@gmail.com>
2023-06-05 15:55:27 -0700

    UI Changes

--- app/src/main/java/com/example/fanzone/ui/FavScreen.kt
+++ app/src/main/java/com/example/fanzone/ui/FavScreen.kt
@@ -1,10 +1,14 @@
 package com.example.fanzone.ui
 
 import UserStorage
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -20,13 +24,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
 import com.example.fanzone.data.DataSource.teamAbbreviations
 import com.google.gson.Gson
 import com.google.gson.JsonElement
+import com.google.gson.JsonPrimitive
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -38,40 +46,37 @@ import java.util.TimeZone
 
 @Composable
 fun FavScreen(dataRepository: UserStorage) {
+    val context = LocalContext.current
     val viewModel: FavScreenViewModel = viewModel()
     val isLoading = viewModel.isLoading.value
     val apiResponse = viewModel.apiResponse.value
-    //val favoriteTeams: List<String> = listOf("atl", "nyy", "lad", "ari")
     val stringList = remember { dataRepository.getStringList() }
     val mappedStrings = stringList.sorted().map { teamName ->
         teamAbbreviations[teamName] ?: ""
     }
+    print(mappedStrings.toString())
     val baseUrl = "https://site.api.espn.com/apis/site/v2/sports/baseball/mlb/teams/"
     val currentIndex = remember { mutableStateOf(0) }
     val favoriteTeamData: MutableMap<String, MutableMap<String, String>> = remember { mutableMapOf() }
 
     LaunchedEffect(Unit) {
         if (currentIndex.value < mappedStrings.size) {
-            //val teamAbb=teamAbbreviations[currentIndex.value]
             viewModel.makeApiCall(baseUrl + mappedStrings[currentIndex.value])
-
         }
-
-
-
     }
 
     val responseJson = Gson().fromJson(
         apiResponse,
         JsonElement::class.java
     )
+
     if (responseJson != null){
         val teamData: MutableMap<String, String> = mutableMapOf()
         val jsonElement = responseJson.asJsonObject["team"].asJsonObject
         teamData["teamName"] = jsonElement["displayName"].asString
         teamData["record"] = jsonElement["record"].asJsonObject["items"].asJsonArray
             .firstOrNull { it.asJsonObject["description"].asString == "Overall Record" }
-            ?.asJsonObject?.get("summary").toString()
+            ?.asJsonObject?.get("summary").toString().replace("\"", "")
 
         // Extracting next event and short name
         val nextEventArray = jsonElement["nextEvent"].asJsonArray
@@ -87,10 +92,17 @@ fun FavScreen(dataRepository: UserStorage) {
             teamData["nextEventName"] = nextEvent["shortName"].asString
         }
 
-
         // Extracting team logo
         val logoUrl = jsonElement["logos"].asJsonArray[0].asJsonObject["href"].asString
         teamData["logoUrl"] = logoUrl
+
+        // Extracting roster link
+        val rosterLink = jsonElement["links"].asJsonArray
+            .firstOrNull { it.asJsonObject["rel"].asJsonArray.contains(JsonPrimitive("roster")) }
+            ?.asJsonObject?.get("href")?.asString
+        if (rosterLink != null) {
+            teamData["rosterLink"] = rosterLink
+        }
 
         favoriteTeamData[jsonElement["abbreviation"].asString.lowercase()] = teamData
         currentIndex.value++
@@ -101,14 +113,21 @@ fun FavScreen(dataRepository: UserStorage) {
 
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
         if (favoriteTeamData.isEmpty()) {
-            Text(text = "Loading...", style = MaterialTheme.typography.h5)
+            Text(text = "No Favorite Teams Found", style = MaterialTheme.typography.h5)
         } else {
             for (team in mappedStrings) {
                 if (favoriteTeamData[team] != null) {
-                    Card(shape = RoundedCornerShape(10.dp), modifier = Modifier.padding(10.dp)) {
-                        Row(verticalAlignment = Alignment.CenterVertically) {
+                    Card(
+                        shape = RoundedCornerShape(10.dp),
+                        modifier = Modifier.padding(10.dp),
+                        elevation = 8.dp // Add shadow effect
+                    ) {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier.padding(16.dp) // Padding around elements inside card
+                        ) {
                             val imageUrl = favoriteTeamData[team]?.get("logoUrl")!!
-                            val imagePainter = rememberAsyncImagePainter(model = imageUrl)
+                            val imagePainter = rememberImagePainter(data = imageUrl)
                             Image(
                                 painter = imagePainter,
                                 contentDescription = "${favoriteTeamData[team]?.get("teamName")!!} Logo",
@@ -118,11 +137,31 @@ fun FavScreen(dataRepository: UserStorage) {
                             )
                             Spacer(modifier = Modifier.width(16.dp))
                             Column {
-                                Text(text = favoriteTeamData[team]?.get("teamName")!!, style = MaterialTheme.typography.h5)
-                                Text(text = "Overall Record: ${favoriteTeamData[team]?.get("record")}", style = MaterialTheme.typography.body1)
-                                Text(text = "Next Event: ${favoriteTeamData[team]?.get("nextEventDate")}, " +
-                                        "${favoriteTeamData[team]?.get("nextEventName")}", style = MaterialTheme.typography.body1)
-
+                                Text(
+                                    text = favoriteTeamData[team]?.get("teamName")!!,
+                                    style = MaterialTheme.typography.h4.copy(color = MaterialTheme.colors.secondary) // Increased size, added color
+                                )
+                                Text(
+                                    text = "Overall Record: ${favoriteTeamData[team]?.get("record")}",
+                                    style = MaterialTheme.typography.body1,
+                                )
+                                Text(
+                                    text = "Next Event: ${favoriteTeamData[team]?.get("nextEventDate")}, ${favoriteTeamData[team]?.get("nextEventName")}",
+                                    style = MaterialTheme.typography.body1
+                                )
+                                val rosterLink = favoriteTeamData[team]?.get("rosterLink")
+                                if (rosterLink != null) {
+                                    Text(
+                                        text = "Roster",
+                                        modifier = Modifier.clickable {
+                                            context.startActivity(
+                                                Intent(Intent.ACTION_VIEW, Uri.parse(rosterLink))
+                                            )
+                                        },
+                                        style = MaterialTheme.typography.h5.copy(color = Color.Black) // Increased size, added color
+                                    )
+                                }
+                                Spacer(modifier = Modifier.height(8.dp)) // Add space at the end of each card
                             }
                         }
                     }
