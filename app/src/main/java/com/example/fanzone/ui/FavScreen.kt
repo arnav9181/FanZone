446fa2731011f396b7c8ee4ac60f4564af1f080b
arnav9181 <arnavbanthia@gmail.com>
2023-06-06 16:26:30 -0700

    Added prices and number of tickets with link

--- app/src/main/java/com/example/fanzone/ui/FavScreen.kt
+++ app/src/main/java/com/example/fanzone/ui/FavScreen.kt
@@ -105,6 +105,20 @@ fun FavScreen(dataRepository: UserStorage) {
             val outputFormat = SimpleDateFormat("MM-dd-yyyy", Locale.ENGLISH)
             val date = inputFormat.parse(dateStr)
             val formattedDateStr = outputFormat.format(date)
+            val competitions = nextEvent["competitions"].asJsonArray
+            val competitions1 = competitions[0].asJsonObject
+            val tickets = competitions1["tickets"].asJsonArray
+            val ticketPrices = tickets[0].asJsonObject
+            val availTickets = ticketPrices["numberAvailable"].asString
+            val finalPrices = ticketPrices["summary"].asString
+            val ticketsRemaining = ticketPrices["numberAvailable"].asString
+            val ticketsLink0 = ticketPrices["links"].asJsonArray
+            val ticketsLink1 = ticketsLink0[0].asJsonObject
+            val ticketsLink2 = ticketsLink1["href"].asString
+            teamData["availTickets"] = availTickets
+            teamData["ticketsLink"] = ticketsLink2
+            teamData["ticketPrices"] = finalPrices
+            teamData["ticketsRemaining"] = ticketsRemaining
             teamData["nextEventDate"] = formattedDateStr
             teamData["nextEventName"] = nextEvent["shortName"].asString
         }
@@ -144,7 +158,7 @@ fun FavScreen(dataRepository: UserStorage) {
                             modifier = Modifier.padding(16.dp)
                         ) {
                             val imageUrl = teamData["logoUrl"]!!
-                            val imagePainter = rememberImagePainter(data = imageUrl)
+                            val imagePainter = rememberAsyncImagePainter(model = imageUrl)
                             Image(
                                 painter = imagePainter,
                                 contentDescription = "${teamData["teamName"]!!} Logo",
@@ -178,6 +192,26 @@ fun FavScreen(dataRepository: UserStorage) {
                                     text = "Next Event: ${teamData["nextEventDate"]}, ${teamData["nextEventName"]}",
                                     style = MaterialTheme.typography.body1
                                 )
+                                Text(
+                                    text = "Ticket Prices: ${teamData["ticketPrices"]}",
+                                    style = MaterialTheme.typography.body1
+                                )
+                                Text(
+                                    text = "Tickets Available: ${teamData["availTickets"]}",
+                                    style = MaterialTheme.typography.body1
+                                )
+                                val linkers = teamData["ticketsLink"]
+                                if (linkers != null) {
+                                    Text(
+                                        text = "Buy Tickets Here",
+                                        modifier = Modifier.clickable {
+                                            context.startActivity(
+                                                Intent(Intent.ACTION_VIEW, Uri.parse(linkers))
+                                            )
+                                        },
+                                        style = MaterialTheme.typography.h5.copy(color = Color.Black)
+                                    )
+                                }
                                 val rosterLink = teamData["rosterLink"]
                                 if (rosterLink != null) {
                                     Text(
